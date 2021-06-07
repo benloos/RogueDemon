676a60b2b6b214464cdad154723def7c0ad19e66
Admiral1712 <nikolas.klanten@alumni.fh-aachen.de>
2021-06-07 16:17:29 +0200

    Player-Weapon hinzugefügt
    hinzugrügt:
    -waffe
    -bewegung der waffe zum bewegen
    fehlt:
    -vertikale bewegung der waffe mit der kamera

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,15 +10,22 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float speed = 12f;
     [SerializeField] private CapsuleCollider groundCheck;
     [SerializeField] private float gravity = -12f; //-9.81
-    public float groundDistance = 0.4f;
+    [SerializeField] private GameObject weapon;
     [SerializeField] private Vector3 velocity;
     [SerializeField] private float jumpforce = 10f;
+    public float groundDistance = 0.4f;
     private float orig_height;
     private float crouch_height;
+    private float normal_extend = 0.1f;
+    private float crouch_extend = 1.6f;
+    private Vector3 weaponOrigin;
+    private float movementCounter;
+    private float idleCounter;
 
     void Start(){
         orig_height = controller.height;
         crouch_height = controller.height/2;
+        weaponOrigin = weapon.transform.localPosition;
     }
 
     // Update is called once per frame
@@ -42,7 +49,7 @@ public class PlayerController : MonoBehaviour
         While not on the ground gravity is added with each frame to increase the speed at which we are falling
          */
         if(IsGrounded()){
-            if(Input.GetKey(KeyCode.Space)){
+            if(Input.GetKeyDown(KeyCode.Space)){
                 // give upward impulse
                 velocity.y = jumpforce;
             }
@@ -51,13 +58,41 @@ public class PlayerController : MonoBehaviour
             velocity.y = Mathf.Clamp(velocity.y, gravity, jumpforce);
         }
         controller.Move(velocity * Time.deltaTime);
+        
 
-        if(Input.GetKey(KeyCode.LeftControl)){
-            //transform.localScale.Set() = Mathf.Lerp(transform.localScale.y,  1f, 5*Time.deltaTime);
-        }else{
+        /**
+        wenn LeftControl gehalten wird wird die hitbox des Spielers kleiner um crouchen zu erzeugen
+        wird LeftControl wieder losgelassen wird die spielfigur auf die ursprüngliche höhe des charakters + 0.1
+        !!! damit der bums hier richtig funktioniert muss man sich beim crouchen bewegen sonst bleibt der im boden stecken... !!!
+        */
+        if(Input.GetKeyDown(KeyCode.LeftControl)){
+            controller.height = crouch_height;
+            groundCheck.height = crouch_height;
+        }else if(Input.GetKeyUp(KeyCode.LeftControl)){
+            Vector3 temp = transform.position;
+            temp.y = 0.1f; 
+            transform.position = temp;
+            groundCheck.height = orig_height;
             controller.height = orig_height;
         }
-       
+
+        if(Input.GetKeyDown(KeyCode.LeftShift)){
+            speed = 18f;
+        } else if(Input.GetKeyUp(KeyCode.LeftShift)){
+            speed = 12f;
+        }
+        
+        //Debug.Log("Weapon Origin: (" + weaponOrigin.x + ", " + weaponOrigin.y + ", " + weaponOrigin.z + ")");
+        //HeadBob
+        if(x == 0 && z == 0){
+            HeadBob(idleCounter, 0.025f, 0.025f);
+            idleCounter += Time.deltaTime;
+            //Debug.Log("we are standing still");
+        } else {
+            HeadBob(movementCounter, 0.035f, 0.035f);
+            movementCounter += Time.deltaTime * 3.5f;
+            //Debug.Log("moveing");
+        }
     }
 
     // check for floor or other object beneath the player with Raycast
@@ -69,6 +104,10 @@ public class PlayerController : MonoBehaviour
             return true;
         }
         return false;
+    }
+
+    private void HeadBob (float z, float x_intensity, float y_intensity){
+        weapon.transform.localPosition = weaponOrigin + new Vector3(Mathf.Cos(z) * x_intensity, Mathf.Sin(z * 2) * y_intensity, 0);
     }
 }
 
