fb1e7ca1436014dd56087c009b19646fb69a50fb
Admiral1712 <nikolas.klanten@alumni.fh-aachen.de>
2021-08-04 17:39:53 +0200

    recoil eingebaut aber der ist noch scheiße

--- Assets/shoot_wumms.cs
+++ Assets/shoot_wumms.cs
@@ -11,12 +11,14 @@ public class shoot_wumms : MonoBehaviour
     [SerializeField] private Camera cam;
     [SerializeField] private Transform attackPoint;
     [SerializeField] private float shootForce;
+    [SerializeField] private Vector3 upRecoil;
+    private Vector3 originalPosition;
     // Start is called before the first frame update
     void Start()
     {
-        //Debug.Log("AttackPosition: " + attackPoint.localPosition);
         attackPoint.localPosition.Set(0.015f, 0, -0.788f);
-        Debug.Log("AttackPosition: " + attackPoint.localPosition);
+        originalPosition = transform.localEulerAngles;   
+        
     }
 
     // Update is called once per frame
@@ -27,11 +29,16 @@ public class shoot_wumms : MonoBehaviour
             MuzzleFlash_Left.Play();
             MuzzleFlash_Right.Play();
             shoot();
+            Recoil();
+            
+        } else if(Input.GetKeyUp(KeyCode.Mouse0)){
+            StopRecoil();
         }
     }
 
 
     private void shoot(){
+        Debug.Log(originalPosition);
         //hit position finden
         Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
         RaycastHit hit;
@@ -43,10 +50,10 @@ public class shoot_wumms : MonoBehaviour
         else
             targetPoint = ray.GetPoint(75); //Just a point far away from the player
 
-        Debug.Log("Raycast hit: " + hit.collider.gameObject.name);
+        //Debug.Log("Raycast hit: " + hit.collider.gameObject.name);
         //Calculate direction from attackPoint to targetPoint
         Vector3 directionWithoutSpread = targetPoint - attackPoint.position;
-        Debug.Log("AttackPosition: " + attackPoint.localPosition);
+        //Debug.Log("AttackPosition: " + attackPoint.localPosition);
         //Instantiate bullet/projectile
         GameObject currentBullet = Instantiate(projectile, attackPoint.position, Quaternion.identity); //store instantiated bullet in currentBullet
         //Rotate bullet to shoot direction
@@ -54,5 +61,16 @@ public class shoot_wumms : MonoBehaviour
 
                 //Add forces to bullet
         currentBullet.GetComponent<Rigidbody>().AddForce(directionWithoutSpread.normalized * shootForce, ForceMode.Impulse);
+
+
+    }
+
+    private void Recoil(){
+        Debug.Log("Hellooo");
+        transform.localEulerAngles = Vector3.Lerp(transform.localEulerAngles, upRecoil, 0.5f);
+    }
+
+    private void StopRecoil(){
+        transform.localEulerAngles = Vector3.Lerp(transform.localEulerAngles, originalPosition, 0.5f);
     }
 }
