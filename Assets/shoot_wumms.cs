dd3fa125d275544dc61de1dff6086bc0cd6cc850
Admiral1712 <nikolas.klanten@alumni.fh-aachen.de>
2021-08-04 12:41:53 +0200

    muzzleflash und start vom projectile

--- Assets/shoot_wumms.cs
+++ Assets/shoot_wumms.cs
@@ -7,6 +7,8 @@ public class shoot_wumms : MonoBehaviour
     [SerializeField] private ParticleSystem MuzzleFlash_Front;
     [SerializeField] private ParticleSystem MuzzleFlash_Left;
     [SerializeField] private ParticleSystem MuzzleFlash_Right;
+    [SerializeField] private GameObject projectile;
+    [SerializeField] private Camera cam;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,10 +18,35 @@ public class shoot_wumms : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.Mouse0)){
+        if(Input.GetKeyDown(KeyCode.Mouse0)){
+
             MuzzleFlash_Front.Play();
             MuzzleFlash_Left.Play();
             MuzzleFlash_Right.Play();
+            spawn_projectile();
         }
+    }
+
+    private void spawn_projectile(){
+        //Vector3 local_position = new Vector3(0.0171073f, -0.00849f, -0.836298f);
+        Vector3 worldposition = transform.TransformPoint(Vector3.forward * 1.5f);
+        Instantiate(projectile, worldposition, Quaternion.identity);
+        //temp.transform.localPosition = local_position;
+    }
+
+    private void shoot(){
+        //hit position finden
+        Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+
+        //check if ray hits something
+        Vector3 targetPoint;
+        if (Physics.Raycast(ray, out hit))
+            targetPoint = hit.point;
+        else
+            targetPoint = ray.GetPoint(75); //Just a point far away from the player
+
+        //Calculate direction from attackPoint to targetPoint
+        Vector3 directionWithoutSpread = targetPoint - attackPoint.position;
     }
 }
